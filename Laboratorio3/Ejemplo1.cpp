b65d5a0567ee1cf82538f574f6709920045076c2
FR0K3 <00174320@uca.edu.sv>
2020-05-20 12:35:49 -0600

    Labo3

---
+++ Laboratorio3/Ejemplo1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n1, n2;
+    cout << "Ingrese el primer numero:";
+    cin >> n1;
+    cout << "Ingrese el segundo numero:";
+    cin >> n2;
+
+    if (n1 > n2){
+        cout << n1 << " es mayor que " << n2;
+    }
+    else if(n1 < n2) {
+        cout << n1 << " es menor que " << n2;
+    } else{
+        cout << n1 << " es igual a " << n2;
+    }
+
+}
